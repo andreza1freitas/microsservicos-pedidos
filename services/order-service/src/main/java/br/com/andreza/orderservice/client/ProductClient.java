ce11781c663e6d3273524e472ea05a31b32791d5
andreza.freitas <andreza1freitas@gmail.com>
2026-03-06 17:30:53 -0300

    feat: containerização dos microsservicos com Docker e Docker Compose

--- services/order-service/src/main/java/br/com/andreza/orderservice/client/ProductClient.java
+++ services/order-service/src/main/java/br/com/andreza/orderservice/client/ProductClient.java
@@ -4,7 +4,7 @@ import br.com.andreza.orderservice.dto.ProductResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "product-service", url = "http://localhost:8082")
+@FeignClient(name = "product-service", url = "http://product-service:8082")
 public interface ProductClient {
 
     @GetMapping("/products/{id}")
--- services/order-service/src/main/java/br/com/andreza/orderservice/controller/OrderController.java
+++ services/order-service/src/main/java/br/com/andreza/orderservice/controller/OrderController.java
@@ -6,7 +6,6 @@ import br.com.andreza.orderservice.service.OrderService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
